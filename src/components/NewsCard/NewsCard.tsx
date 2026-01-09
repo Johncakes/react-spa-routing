722c9f6d054743110526173b6c75ef82505aeca8
Johncakes <johnnygamesover9000@gmail.com>
2026-01-09 18:52:29 +0900

    feat: Header, News Cards, API Calls, React-router setup
    - Added mobile header, pc Header styles
    - Added news cards to use cropped images and have a set styling
    - API Calls with tanstack query
    - Made a single Router to manage different categories

---
+++ src/components/NewsCard/NewsCard.tsx
@@ -0,0 +1,42 @@
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { AspectRatio } from "../ui/aspect-ratio";
+
+interface NewsCardProps {
+  Title?: string;
+  imageUrl?: string | null;
+  description?: string | null;
+  link?: string;
+}
+
+export default function NewsCard({
+  Title,
+  imageUrl,
+  description,
+  link,
+}: NewsCardProps) {
+  return (
+    <a href={link} target="_blank" rel="noreferrer" className="block">
+      <Card className="overflow-hidden h-full flex flex-row transition-all hover:shadow-lg px-6">
+        {imageUrl && (
+          <div className="w-40 h-40 shrink-0 rounded-md overflow-hidden">
+            <AspectRatio ratio={1 / 1}>
+              <img
+                src={imageUrl}
+                alt={Title ?? ""}
+                className="w-full h-full object-cover"
+              />
+            </AspectRatio>
+          </div>
+        )}
+        <div className="flex flex-col flex-1">
+          <CardTitle className="line-clamp-2 mb-2">{Title}</CardTitle>
+          <CardContent className="flex-1 p-0">
+            <p className="text-sm text-muted-foreground line-clamp-3">
+              {description}
+            </p>
+          </CardContent>
+        </div>
+      </Card>
+    </a>
+  );
+}
---
+++ src/routes.ts
@@ -0,0 +1,10 @@
+export const ROUTES = {
+  HOME: "/",
+  BUSINESS: "/business",
+  ENTERTAINMENT: "/entertainment",
+  GENERAL: "/general",
+  HEALTH: "/health",
+  SCIENCE: "/science",
+  SPORT: "/sports",
+  TECHNOLOGY: "/technology",
+} as const;
